65131e202495ff62583bd5a6db7a579f646226c3
Faizan Janjua <faizanhucane3332@gmail.com>
2026-03-16 22:09:12 -0700

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,9 @@
 const express = require('express');
+const cors = require('cors');
+
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
@@ -9,10 +12,7 @@ app.get('/', (req, res) => {
 });
 
 app.get('/health', (req, res) => {
-  res.status(200).json({
-    ok: true,
-    message: 'Viddrop backend is healthy'
-  });
+  res.status(200).json({ ok: true });
 });
 
 const downloadRoute = require('./routes/download');
@@ -22,7 +22,4 @@ const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
-
-const infoRoute = require('./routes/info');
-app.use('/info', infoRoute);
 });
