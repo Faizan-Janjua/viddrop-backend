71f2ad1078ef25f01bc899d721df01463e65b8d6
Faizan Janjua <faizanhucane3332@gmail.com>
2026-03-16 20:59:51 -0700

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('OK');
+  res.status(200).send('OK');
 });
 
 const PORT = process.env.PORT || 3000;
